7629f55c789f8e20885f71e1eebfcb58b7ae6f99
Aleksandr Naumtsev <sashadaanton@mail.ru>
2023-05-12 03:30:57 +0300

    use-case: add use-case snippet_for_find_tags.py

---
+++ ranking/ranking_system/use_cases/snippet_for_find_tags.py
@@ -0,0 +1,21 @@
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from rankingSystem import RankingSystem
+
+if __name__ == "__main__":
+    print("[*] Service RankingSystem start work! [*]\n")
+    ranking_system = RankingSystem()
+
+    ranking_system.update_courses()
+    ranking_system.update_tags()
+    ranking_system.update_users()
+
+    req = "Алгебраические кольца"
+    tags = ranking_system.get_top_nearest_tags(req=req)
+    print(tags)
+    
+    tags = ranking_system.get_top_nearest_tags(req=req, metric_func=RankingSystem._inv_levenshtain_ratio)
+    print(tags)
